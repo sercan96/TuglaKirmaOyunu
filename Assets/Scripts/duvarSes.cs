b139c82adeb5de511d02f04e245988206508dd34
SERCAN <sercan_saglam19@hotmail.com>
2021-11-14 19:09:23 +0300

    commit

--- Assets/Scripts/duvarSes.cs
+++ Assets/Scripts/duvarSes.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class duvarSes : MonoBehaviour
 {
     public AudioClip duvarSesi;
+    public GameObject particleEfekt;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GetComponent<AudioSource>().PlayOneShot(duvarSesi);
+        Vector3 pos = collision.contacts[0].point;
+        GameObject go = Instantiate(particleEfekt, pos, Quaternion.identity);
+        Destroy(go, 1f);
+
     }
 }
