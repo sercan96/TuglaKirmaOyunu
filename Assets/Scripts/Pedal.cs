d9538f6103f6ac3692ccff548d1e83a5af005511
SERCAN <sercan_saglam19@hotmail.com>
2021-11-14 20:43:11 +0300

    commit

--- Assets/Scripts/Pedal.cs
+++ Assets/Scripts/Pedal.cs
@@ -14,8 +14,8 @@ public class Pedal : MonoBehaviour
     {
         Vector3 farepos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
         //transform.position = farepos; // Mouse nereye hareket ettirirsem pedal objem o yöne doðru hareket edecektir.
-        //transform.position = new Vector3(Mathf.Clamp(farepos.x, -3f, 3f), transform.position.y, transform.position.z); //Sadece x yönünde mouse ile hareket etmeyi saðlar.
-        transform.position = new Vector3(top.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(farepos.x, -3f, 3f), transform.position.y, transform.position.z); //Sadece x yönünde mouse ile hareket etmeyi saðlar.
+        //transform.position = new Vector3(top.transform.position.x, transform.position.y, transform.position.z);
         //Bu kod ile pedal topu kendiliðinden takip edeceði için hiçbir þekilde ölmez.
     }
 }
